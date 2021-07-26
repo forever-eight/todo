e54ad34a7fcf85b6e61cb92d3f45e5063db2803f
Marie <mariatsikina008@gmail.com>
2021-07-26 15:48:53 +0300

    started migrations + postgres

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 
+	"github.com/spf13/viper"
+
 	"github.com/forever-eight/todo.git"
 	"github.com/forever-eight/todo.git/internal/app/handler"
 	"github.com/forever-eight/todo.git/internal/app/repository"
@@ -10,12 +12,22 @@ import (
 )
 
 func main() {
+	err := initConfig()
+	if err != nil {
+		log.Fatalf("config initialization error: %s", err)
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	err := srv.Run("8080", handlers.InitRoutes())
+	err = srv.Run(viper.GetString("port"), handlers.InitRoutes())
 	if err != nil {
 		log.Println("error occurred while running http server ", err)
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/forever-eight/todo.git
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.2 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/spf13/viper v1.8.1
+
+)
